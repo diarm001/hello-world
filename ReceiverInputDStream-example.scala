0b53e9a3a99408749c9ee67f2565d42fbc0707e7
diarm001 <diarm1@protonmail.com>
2016-08-29 14:34:15 +0100

    Update ReceiverInputDStream-example.scala

--- ReceiverInputDStream-example.scala
+++ ReceiverInputDStream-example.scala
@@ -7,3 +7,13 @@ val conf = new SparkConf(false) // skip loading external settings
   .set("spark.akka.logLifecycleEvents", "true")
 
 val ssc = new StreamingContext(conf, Seconds(1))
+
+
+//This gives you a context to access the actor system that is of type ReceiverInputDStream. 
+
+//Having DStream lets you define a high-level processing pipeline in Spark Streaming. 
+
+//actorStream.print()
+
+//class Diarm1 extends Actor with ActorHelper {
+//val actorStream = ssc.actorStream[String](Props[Diar], actorName)
