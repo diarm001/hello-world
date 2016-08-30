10ed03778cb3e01763a4c2c338db56a6af4480e1
diarm001 <diarm1@protonmail.com>
2016-08-30 13:01:05 +0100

    Add files via upload

---
+++ EnumAnimal.scala
@@ -0,0 +1,21 @@
+// EnumAnimal.scala
+// This is an example of a Schema Definition programmed in Scala to 
+// be used as GraphQL to chat with scala
+// In our case Akka
+// Defines enumerated data types for cat, dog + goat.
+
+object SchemaDefinition {
+  val EnumAnimal = EnumType(
+    "Animal",
+    Some("What could the animal be?"),
+    List(
+      EnumValue("CAT",
+        value = Animal.CAT,
+        description = Some("Meow.")),
+      EnumValue("DOG",
+        value = Animal.DOG,
+        description = Some("Bow Wow.")),
+      EnumValue("GOAT",
+        value = Animal.JEDI,
+        description = Some("Fancy some milk?"))))
+}
