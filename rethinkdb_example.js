a2fa2a08b720a4a8d5f54f75a95377a4f3cb19a2
diarm001 <diarm1@protonmail.com>
2016-09-14 15:10:47 +0100

    Update rethinkdb_example.js

--- rethinkdb_example.js
+++ rethinkdb_example.js
@@ -4,6 +4,16 @@ $> rethinkdb
 
 //create a table
 
+//Create connection, kick off node and define the variable "connection" to work with node
+
+r = require('rethinkdb');
+
+var connection = null;
+r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+    if (err) throw err;
+    conn = connection ; 
+})
+
 r.db('DB_NAME').tableCreate('TABLE_NAME')
 
 // Insert some JSON into the table
