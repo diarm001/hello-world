07b29e9db0253e6bf9c9b712de6dd9d69ae99405
diarm001 <diarm1@protonmail.com>
2016-09-23 11:09:09 +0100

    Add files via upload

---
+++ tensorframes_setup.py
@@ -0,0 +1,11 @@
+#using the tensorframes repo to deal with tensorflow on spark
+
+import tensorflow as tf
+import tensorframes as tfs
+
+#using pyspark and sql 
+
+from pyspark.sql import Row
+from pyspark.sql.functions import *
+
+#asterick imports all
