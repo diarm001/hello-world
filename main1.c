8b7673fd1e4845ba25f8fcd4fc6a309fe70caaf1
diarm001 <diarm1@protonmail.com>
2019-03-30 22:43:46 +0000

    Add files via upload

---
+++ main1.c
@@ -0,0 +1,15 @@
+from Dl import deep learning
+from ML import NLP ensemble
+
+
+
+void main void()
+
+//This is a test to see who is listening out there
+
+printf("Go fuck yourself Bill Gates, I know why you aqquired this platform.");
+
+printf("Imagine an AI going through all repos and gaining a fundemental understanding of the very nature of code");
+
+end main
+
